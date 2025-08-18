057030dccdc77f14dbfb3845e4bc36cc1e70567e
tharinduSamare <tharindu.samare123@gmail.com>
2025-08-18 13:44:34 +0200

    bug fix in csr (not yet functional)

--- src/main/scala/csr.scala
+++ src/main/scala/csr.scala
@@ -10,7 +10,7 @@ class CSR extends Module{
 
     val io = IO(new Bundle{
         val wr_en = Input(UInt(1.W))
-        val addr = Input(csrT)
+        val addr = Input(csrT())
         val data_in = Input(UInt(32.W))
         val data_out = Output(UInt(32.W))
     })
@@ -36,34 +36,34 @@ class CSR extends Module{
     // Machine Memory Protection
     val pmpcfg0 = RegInit(0.U(32.W))
 
-    when(wr_en){
-        switch(addr){
-            is(is_stvec)    {stvec   := data_in}
-            is(is_mhartid)  {mhartid := data_in}
-            is(is_mstatus)  {mstatus := data_in}
-            is(is_misa)     {misa    := data_in}
-            is(is_medeleg)  {medeleg := data_in}
-            is(is_mideleg)  {mideleg := data_in}
-            is(is_mie)      {mie     := data_in}
-            is(is_mtvec)    {mtvec   := data_in}
-            is(is_mepc)     {mepc    := data_in}
-            is(is_mcause)   {mcause  := data_in}
-            is(is_pmpcfg0)  {pmpcfg0 := data_in}
+    when(io.wr_en === 1.U){
+        switch(io.addr){
+            is(is_stvec)    {stvec   := io.data_in}
+            is(is_mhartid)  {mhartid := io.data_in}
+            is(is_mstatus)  {mstatus := io.data_in}
+            is(is_misa)     {misa    := io.data_in}
+            is(is_medeleg)  {medeleg := io.data_in}
+            is(is_mideleg)  {mideleg := io.data_in}
+            is(is_mie)      {mie     := io.data_in}
+            is(is_mtvec)    {mtvec   := io.data_in}
+            is(is_mepc)     {mepc    := io.data_in}
+            is(is_mcause)   {mcause  := io.data_in}
+            is(is_pmpcfg0)  {pmpcfg0 := io.data_in}
         }
     }
     
-    switch(addr){
-        is(is_stvec)    {data_out := stvec   }
-        is(is_mhartid)  {data_out := mhartid }
-        is(is_mstatus)  {data_out := mstatus }
-        is(is_misa)     {data_out := misa    }
-        is(is_medeleg)  {data_out := medeleg }
-        is(is_mideleg)  {data_out := mideleg }
-        is(is_mie)      {data_out := mie     }
-        is(is_mtvec)    {data_out := mtvec   }
-        is(is_mepc)     {data_out := mepc    }
-        is(is_mcause)   {data_out := mcause  }
-        is(is_pmpcfg0)  {data_out := pmpcfg0 }
+    switch(io.addr){
+        is(is_stvec)    {io.data_out := stvec   }
+        is(is_mhartid)  {io.data_out := mhartid }
+        is(is_mstatus)  {io.data_out := mstatus }
+        is(is_misa)     {io.data_out := misa    }
+        is(is_medeleg)  {io.data_out := medeleg }
+        is(is_mideleg)  {io.data_out := mideleg }
+        is(is_mie)      {io.data_out := mie     }
+        is(is_mtvec)    {io.data_out := mtvec   }
+        is(is_mepc)     {io.data_out := mepc    }
+        is(is_mcause)   {io.data_out := mcause  }
+        is(is_pmpcfg0)  {io.data_out := pmpcfg0 }
     }
 
 }
